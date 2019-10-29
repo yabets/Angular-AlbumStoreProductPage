ecfbc7b5aabc361342de8ecbe6f9c12b7904ce11
yabets <yabetsbelay@gmail.com>
2019-10-30 00:32:30 +0530

    6. Displaying a List of Products soln

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,13 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private  _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -16,4 +18,8 @@ export class ProductService {
               .map(response => <Album> response.json());
    }
 
+   getProducts(): Observable<Product[]> {
+     return this._http.get(this._productsUrl)
+                  .map(response => <Product[]>response.json());
+   }
 }
